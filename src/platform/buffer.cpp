8ed7fbf90e9c937620a82af113575f13c3cec69c
Arktetra <sameershrestha.sm@gmail.com>
2024-08-03 21:35:53 +0545

    change: abstraction to function oriented

--- src/platform/buffer.cpp
+++ src/platform/buffer.cpp
@@ -1,75 +1,47 @@
 #include "./buffer.hpp"
 
-/**
- * @brief create a vertex buffer object
- * 
- * @param vertices data to draw.
- * @param draw_type specifies the graphics card how to manage the given data.
- */
 void Buffer::create_VBO(std::vector<float> vertices, int draw_type) {
-    glGenBuffers(1, &this->VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+    unsigned VBO;
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), draw_type);
 }
 
-/**
- * @brief create a vertex array object.
- */
-void Buffer::create_VAO() {
-    glGenVertexArrays(1, &this->VAO);
-    glBindVertexArray(this->VAO);
+unsigned Buffer::create_VAO(std::vector<float> vertices, int draw_type) {
+    Buffer::create_VBO(vertices, draw_type);
+    
+    unsigned VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    return VAO;
 }
 
-/**
- * @brief create am element buffer object.
- * 
- * @param indices index of the data to draw.
- * @param draw_type specifies the graphics card how to manage the given data.
- */
 void Buffer::create_EBO(std::vector<int> indices, int draw_type) {
-    glGenBuffers(1, &this->EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+    unsigned EBO;
+
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * indices.size(), indices.data(), draw_type);
 }
 
-/**
- * @brief Construct a new Buffer:: Buffer object without EBO.
- * 
- * @param vertices data to draw.
- * @param draw_type specifies the graphics card how to manage the given data.
- */
-Buffer::Buffer(std::vector<float> vertices, int draw_type) {
-    create_VBO(vertices, draw_type);
-    create_VAO();
+unsigned Buffer::create_VAO(
+    std::vector<float> vertices,
+    std::vector<int> indices,
+    int draw_type
+) {
+    unsigned VAO = Buffer::create_VAO(vertices, draw_type);
+
+    Buffer::create_EBO(indices, draw_type);
+
+    return VAO;
 }
 
-/**
- * @brief Construct a new Buffer:: Buffer object.
- * 
- * @param vertices data to draw.
- * @param indices index of the data to draw.
- * @param draw_type specifies the graphics card how to manage the given data.
- */
-Buffer::Buffer(std::vector<float> vertices, std::vector<int> indices, int draw_type) {
-    create_VBO(vertices, draw_type);
-    create_VAO();
-    create_EBO(indices, draw_type);
+void Buffer::bind(unsigned VAO) {
+    glBindVertexArray(VAO);
 }
 
-/**
- * @brief bind the vertex array object.
- * 
- */
-void Buffer::bind() {
-    glBindVertexArray(this->VAO);
-}
-
-/**
- * @brief Destroy the Buffer:: Buffer object.
- * 
- */
-Buffer::~Buffer() {
-    glDeleteVertexArrays(1, &this->VAO);
-    glDeleteBuffers(1, &this->VBO);
-    glDeleteBuffers(1, &this->EBO);
+void Buffer::unbind() {
+    glBindVertexArray(0);
 }
--- src/platform/buffer.hpp
+++ src/platform/buffer.hpp
@@ -5,22 +5,12 @@
 
 #include <vector>
 
-class Buffer {
-    unsigned int VBO;
-    unsigned int VAO;
-    unsigned int EBO;
-
+namespace Buffer {
     void create_VBO(std::vector<float> vertices, int draw_type);
-    void create_VAO();
+    unsigned create_VAO(std::vector<float> vertices, int draw_type);
     void create_EBO(std::vector<int> indices, int draw_type);
-
-    public:
-    Buffer(std::vector<float> vertices, int draw_type);
-    Buffer(std::vector<float> vertices, std::vector<int> indices, int draw_type);
-    ~Buffer();
-    void bind();
-    unsigned int get_VBO();
-    unsigned int get_VAO();
-    unsigned int get_EBO();
+    unsigned create_VAO(std::vector<float> vertices, std::vector<int> indices, int draw_type);
+    void bind(unsigned VAO);
+    void unbind();
 };
 #endif
